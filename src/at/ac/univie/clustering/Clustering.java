c1bbb80cafd38c759dfc237e9b0a107a78373ecc
fritziF <florian.fritzi@gmail.com>
2015-10-06 21:23:24 +0200

    add interface

---
+++ src/at/ac/univie/clustering/Clustering.java
@@ -0,0 +1,9 @@
+package at.ac.univie.clustering;
+
+import at.ac.univie.clustering.data.DataWorker;
+
+public interface Clustering {
+	
+	public void readData(DataWorker data) throws Exception;
+
+}
